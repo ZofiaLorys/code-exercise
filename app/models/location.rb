998c69408efe3c65fcf0f745c34fb5ef37faeb48
ZofiaLorys <zosia.lorys@gmail.com>
2020-04-08 11:38:21 +0200

    model validations + tests

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
 class Location < ApplicationRecord
+  validates :slug, presence: true, uniqueness: true
+  validates :density_of_population, presence: true
 end
--- spec/models/location_spec.rb
+++ spec/models/location_spec.rb
@@ -4,8 +4,8 @@ require 'rails_helper'
 
 RSpec.describe Location, type: :model do
   describe 'validations' do
-    let!(:location) { create(:location) }
     it { is_expected.to validate_presence_of(:slug) }
+    it { is_expected.to validate_uniqueness_of(:slug) }
     it { is_expected.to validate_presence_of(:density_of_population) }
   end
 end
