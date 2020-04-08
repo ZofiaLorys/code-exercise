98379e67924a0f29a4dffec93bbc1fa7048b6f85
ZofiaLorys <zosia.lorys@gmail.com>
2020-04-08 14:24:12 +0200

    rubocop

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,7 +1,9 @@
+# frozen_string_literal: true
+
 # This will guess the User class
 FactoryBot.define do
-    factory :location do
-      slug { "cracow" }
-      density_of_population  { 150 }
-    end
+  factory :location do
+    slug { 'cracow' }
+    density_of_population { 150 }
   end
+end
